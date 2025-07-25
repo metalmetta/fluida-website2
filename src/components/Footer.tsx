452bf72e2d4f8ed84e7c64e358c79462d83c990e
metalmetta <143459425+metalmetta@users.noreply.github.com>
2025-07-25 09:11:53 +0200

    up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
       <div className="section-container py-8">
         <div className="flex flex-col items-center">
-          <p className="text-sm text-gray-600 text-center leading-relaxed max-w-4xl">
+          <p className="text-sm text-gray-600 text-center leading-relaxed max-w-8xl">
             © 2025 Fluida Inc. All rights reserved. Fluida Inc. is a financial technology company, not a bank or a money services business. Certain services are provided by our licensed partners across the globe. By creating your account on Fluida, you agree to our terms and conditions, our partners' terms, to all applicable laws and regulations, and agree that you are responsible for compliance with any and all applicable local laws.
           </p>
         </div>
