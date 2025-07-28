3b542295c9d8bd7dd2019a16817ca8eb60822433
metalmetta <143459425+metalmetta@users.noreply.github.com>
2025-07-28 15:12:28 +0200

    up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@
 import React from "react";
 const Footer = () => {
   return (
-      <div className="section-container py-8">
+      <div className="section-container py-6">
         <div className="flex flex-col items-center">
-          <p className="text-sm text-gray-600 text-center leading-relaxed max-w-8xl">
+          <p className="text-sm text-gray-500 text-center leading-relaxed max-w-8xl">
             © 2025 Fluida Inc. All rights reserved. Fluida Inc. is a financial technology company, not a bank or a money services business. Certain services are provided by our licensed partners across the globe. By creating your account on Fluida, you agree to our terms and conditions, our partners' terms, to all applicable laws and regulations, and agree that you are responsible for compliance with any and all applicable local laws.
           </p>
         </div>
